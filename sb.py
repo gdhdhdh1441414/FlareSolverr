516a70c0a1b736112ec006f6171014b489aa6079
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2025-12-31 10:00:52 +0800

    Create sb.py

---
+++ sb.py
@@ -0,0 +1,20 @@
+from seleniumbase import SB
+import time
+import sys
+
+# 设置代理（支持HTTP/HTTPS/SOCKS）
+# proxy_string = "http://127.0.0.1:1080"  # 认证代理
+# proxy_string = "http://host:port"  # 无需认证的代理
+# proxy_string = "socks5://host:port"  # SOCKS5代理
+
+with SB(uc=True, test=True, locale="en", proxy=proxy_string) as sb:
+    url = sys.argv[1]
+    sb.activate_cdp_mode(url)
+    sb.sleep(2)
+    sb.solve_captcha()
+    sb.sleep(12)
+    
+    page_source = sb.get_page_source()
+    timestamp = time.strftime("%Y%m%d_%H%M%S")
+
+    print(page_source)
