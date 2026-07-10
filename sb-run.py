b9e674bcd5354bad07f018cc6680ebe79774dec5
gdhdhdh1441414 <105533517+gdhdhdh1441414@users.noreply.github.com>
2026-07-10 10:07:21 +0800

    Update sb-run.py

--- sb-run.py
+++ sb-run.py
@@ -1,6 +1,6 @@
 from seleniumbase import SB
 
-with SB(uc=True, test=True, locale="en", xvfb=True) as sb:
+with SB(uc=True, test=True, locale="en") as sb:
     url = "https://sharemania.us/threads/evanescence-live-in-s%C3%A3o-paulo-2023-documentary-sanctuary-deluxe-bdrip-1080p.301716/"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
